ef6786bab5a66c33bcb343c3340cc47e5d593823
herculeslyndel <38410965+herculeslyndel@users.noreply.github.com>
2020-12-04 04:19:47 -0500

    d

--- shells/2_sd_card_wifi.sh
+++ shells/2_sd_card_wifi.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-git clone https://github.com/stevedepp/gcp-iot-pipeline.git
-cd gcp-iot-pipeline
 ls /Volumes/boot
 cp ./rpi/ssh /Volumes/boot
 cp ./rpi/wpa_supplicant.conf /Volumes/boot
 ls /Volumes/boot
 nano /Volumes/boot/wpa_supplicant.conf
 
-echo 'please safely eject the SD card, place it into the raspberrypi and wait a few minutes while initial boot occurs.  the ssh and wp_supplicant.conf files are being transfered to their proper directories.  during this process, the blank ssh file enables headless operation and ssh connection.  when the green light stops flashing, try to "ping raspberrypi.local" and stop that with cntrl-c if successful.'
+echo 'please safely eject the SD card, place it into the UNPOWERED raspberrypi. Then plug some power into the raspberrypi, and wait a few minutes while initial boot occurs.  the ssh and wp_supplicant.conf files are being transfered to their proper directories.  during this process, the blank ssh file enables headless operation and ssh connection.  when the green light stops flashing, try to "ping raspberrypi.local" and stop that with cntrl-c if successful.'
